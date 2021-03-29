28e1b19a6839bee14b36f685b887a2ec284ead5a
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-03-29 18:16:59 +0200

    plan todos

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 const { getAllUsers, getUser } = require('../controllers/user');
 
-router.get('/', getAllUsers);
-router.get('/:id', getUser);
+router.get('/', getAllUsers); // TODO - isAdmin (with access to route)
+router.get('/:id', getUser); // TODO - isAdmin (with access to route)
+
+// TODO - post - register
+// TODO - post - login
+// TODO - delete - deleteUser (isAdmin) (with access to route)
+// TODO - put - updateUser (isAdmin) (with access to route)
+// TODO - get - see user profile (with access to route)
+// TODO - put - update user profile (with access to route)
 
 module.exports = router;
