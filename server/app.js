59561f770a69cf0e0f04f8d61738d7972c2a5e8e
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-03-29 17:40:45 +0200

    mongo setup

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-console */
 const express = require('express');
+const bodyParser = require('body-parser');
+const { connectDB } = require('./db/index');
+
+require('dotenv').config();
+
+const routes = require('./routes');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Our API is running...');
 });
 
-const PORT = process.env.PORT || 3000;
+app.use('/api', routes);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+connectDB().then(async () => {
+  app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
+});
