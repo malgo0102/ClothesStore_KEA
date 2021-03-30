edd5b72943398d9e69c800b02975ba1ad6f979d5
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-03-30 12:03:09 +0200

    registering a new user function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,5 +24,25 @@ const getUser = (req, res) => {
   });
 };
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json('This user already exists.');
+    // throw new Error('User already exists.');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  return res.status(201).json({
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: user.generateToken(),
+  });
+});
+
 module.exports.getAllUsers = getAllUsers;
 module.exports.getUser = getUser;
