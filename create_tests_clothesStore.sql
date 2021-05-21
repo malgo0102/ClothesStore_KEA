f3483c43c270afa437c60163198a8f79c7733503
Malgo0102 <mal.wit@hotmail.com>
2021-05-21 11:32:25 +0200

    Added pass comment, added 2 queries to tests SQL

--- create_tests_clothesStore.sql
+++ create_tests_clothesStore.sql
@@ -10,6 +10,16 @@ SELECT * FROM invoices;
 SELECT * FROM cart_items;
 SELECT * FROM favourite_products;
 
+# Get products of selected brand
+SELECT * FROM products WHERE brand_id = (SELECT id FROM brands WHERE name = 'Zara');
+SELECT * FROM products WHERE brand_id = (SELECT id FROM brands WHERE name = 'Chanel');
+
+# Get favourite items of a customer
+SELECT products.name
+FROM products LEFT JOIN favourite_products ON products.id = favourite_products.product_id
+WHERE favourite_products.user_id = 1
+ORDER BY products.name;
+
 # Querying through views
 SELECT * FROM v_users;
 SELECT * FROM v_users_info;
